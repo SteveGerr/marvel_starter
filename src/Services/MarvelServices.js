6f7013e3eab34180ac48b48e45698e8a0d0b7e31
SteveGerr <prikolsamara@mail.ru>
2022-01-24 21:35:28 +0400

    feat: add _baseOffset varible

--- src/Services/MarvelServices.js
+++ src/Services/MarvelServices.js
@@ -4,6 +4,7 @@
 class MarvelService {
     _apiBase = "https://gateway.marvel.com:443/v1/public/"
     _apiKey = "apikey=7446ea1b1fc36115b92d708c4f5d1211"
+    _baseOffset = 210
 
     /** Get resourses */
     getResource = async (url) => {
@@ -17,8 +18,8 @@ class MarvelService {
         return await res.json()
     }
 
-    getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async ( offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter)
     }
 
