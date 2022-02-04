d68fc9a35a83f6ac4db8971eab56fc065e990c20
SteveGerr <prikolsamara@mail.ru>
2022-02-05 00:53:47 +0400

    feat: add getAllComics

--- src/Services/MarvelServices.js
+++ src/Services/MarvelServices.js
@@ -7,15 +7,20 @@ const useMarvelService = () => {
     const _apiBase = "https://gateway.marvel.com:443/v1/public/"
     const _apiKey = "apikey=7446ea1b1fc36115b92d708c4f5d1211"
     const _baseOffset = 210
-
-    const getAllCharacters = async ( offset = _baseOffset) => {
+    /** Загрузка списка персонажей */
+    const getAllCharacters = async ( offset = _baseOffset ) => {
         const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
         return res.data.results.map(_transformCharacter)
     }
-
+    /** Загрузка одного персонажа */
     const getCharacter = async (id) => {
         const res = await request(`${_apiBase}/characters/${id}?${_apiKey}`);
         return _transformCharacter(res.data.results[0]);
+    }
+    /** Загрузка списка комиксов */
+    const getAllComics = async ( offset = _baseOffset ) => {
+        const res = await request(`${_apiBase}comics?limit=9&offset=${offset}&${_apiKey}`);
+        return res.data.results.map(_transformComics)
     }
 
     const _transformCharacter = (char) => {
@@ -30,7 +35,17 @@ const useMarvelService = () => {
         }
     }
 
-    return { loading, error, clearError,  getAllCharacters, getCharacter }
+    const _transformComics = (comics) => {
+        return {
+            id: comics.id,
+            title: comics.title,
+            prices: comics.prices,
+            thumbnail: `${comics.thumbnail.path}.${comics.thumbnail.extension}`,
+            url: comics.urls
+        }
+    }
+
+    return { loading, error, clearError,  getAllCharacters, getCharacter, getAllComics }
 }
 
 
