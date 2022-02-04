97c52657080450d213cb09f1bc9a4366df229e6e
SteveGerr <prikolsamara@mail.ru>
2022-02-05 00:53:04 +0400

    feat: add params

--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -1,10 +1,18 @@
 import { useState, useCallback } from 'react';
-
+/** Универсальный хук для загрузки данных */
 export const useHttp = () => {
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
 
-
+    /**
+     * @param {String} url - адрес запроса
+     * @param {String} method - Метод запроса
+     * @param {String} body - тело запроса
+     * @param {Object} headers - заголовки запроса
+     */
+    /** Запрос на сервер
+     *  useCallback - позволяет сделать запрос только один раз, при монтировании
+     */
     const request = useCallback(async (url, method='GET', body = null, headers = {'Content-type': 'application/json'}) => {
         setLoading(true)
 
