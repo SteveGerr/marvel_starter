5eeba3328c47497dd5e58c43a3100a80e544c8d7
SteveGerr <prikolsamara@mail.ru>
2022-02-05 18:02:42 +0400

    feat: add single comic

--- src/components/singleComic/SingleComic.js
+++ src/components/singleComic/SingleComic.js
@@ -1,19 +1,65 @@
+import { useState, useEffect } from 'react/cjs/react.development';
 import './singleComic.scss';
-import xMen from '../../resources/img/x-men.png';
+import useMarvelService from '../../Services/MarvelServices';
+import ErrorMessage from '../errorMessage/errorMessage'
+import Spinner from '../Spinner/Spinner'
+import Skeleton from '../skeleton/Skeleton'
 
 const SingleComic = () => {
+
+    const [comic, setComic] = useState([]);
+
+    const {getComic, loading, error} = useMarvelService()
+
+    useEffect(() => {
+        onRequest()
+    }, [])
+
+    const onRequest = () => {
+        let id = 98402
+        getComic(id)
+            .then(onComicLoaded)
+    }
+
+    /** One comic loading */
+    const onComicLoaded = (comic) => {
+        setComic(comic);
+    }
+
+    const skeleton = comic || loading || error ? null : <Skeleton/>;
+    const errorMessage = error ? <ErrorMessage/> : null;
+    const spinner = loading ? <Spinner/> : null;
+    const content = !(loading || error || !comic) ? <SingleComicView comic={comic}/> : null;
+
+
     return (
         <div className="single-comic">
-            <img src={xMen} alt="x-men" className="single-comic__img"/>
-            <div className="single-comic__info">
-                <h2 className="single-comic__name">X-Men: Days of Future Past</h2>
-                <p className="single-comic__descr">Re-live the legendary first journey into the dystopian future of 2013 - where Sentinels stalk the Earth, and the X-Men are humanity's only hope...until they die! Also featuring the first appearance of Alpha Flight, the return of the Wendigo, the history of the X-Men from Cyclops himself...and a demon for Christmas!?</p>
-                <p className="single-comic__descr">144 pages</p>
-                <p className="single-comic__descr">Language: en-us</p>
-                <div className="single-comic__price">9.99$</div>
-            </div>
-            <a href="#" className="single-comic__back">Back to all</a>
+            {skeleton}
+            {errorMessage}
+            {spinner}
+            {content}
         </div>
+    )
+}
+
+const SingleComicView = ({comic}) => {
+    const {thumbnail, title, desciption, pageCount, prices, url} = comic
+
+    const displPrice = prices[0].price ? prices[0].price + "$" : "No price"
+
+    return (
+        <>
+            <img src={thumbnail} alt={title} className="single-comic__img"/>
+            <div className="single-comic__info">
+                <h2 className="single-comic__name">{title}</h2>
+                <p className="single-comic__descr">{desciption || "No description"}</p>
+                <p className="single-comic__descr">Pages: {pageCount}</p>
+                <p className="single-comic__descr"></p>
+                <div className="single-comic__price">{displPrice}</div>
+            </div>
+            <a href={url} className="single-comic__back">Back to all</a>
+        </>
+
     )
 }
 
