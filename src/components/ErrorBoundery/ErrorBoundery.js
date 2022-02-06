b10b9f6ab5f8b41e9a1f83c1db95f11cc34499e6
SteveGerr <prikolsamara@mail.ru>
2022-02-06 21:59:11 +0400

    refactor: remove console.log => ErrorBoundry

--- src/components/ErrorBoundery/ErrorBoundery.js
+++ src/components/ErrorBoundery/ErrorBoundery.js
@@ -2,14 +2,13 @@ import { Component } from "react";
 import ErrorMessage from "../errorMessage/errorMessage";
 
 
-// Компонент-обёртка, ловящий ошибки компонентов, находящихся внутри него
+/** Компонент-обёртка, ловящий ошибки компонентов, находящихся внутри него */
 class ErrorBoundary extends Component {
     state = {
         error: false
     }
 
     componentDidCatch(error, errorInfo) {
-        console.log(error, errorInfo);
         this.setState({
             error: true
         })
