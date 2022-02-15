1eb9fc2a51e1c6dde77bb1d6e867f5aa1d54a11d
SteveGerr <prikolsamara@mail.ru>
2022-02-15 23:33:16 +0400

    feat: add new routes for chars and comics

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,6 +2,8 @@ import { lazy, Suspense } from "react";
 import AppHeader from "../appHeader/AppHeader";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Spinner from '../Spinner/Spinner';
+import SinglePage from '../pages/SinglePage';
+import SingleCharPage from '../pages/singleCharPage/SingleCharPage';
 
 const Page404 = lazy(() => import('../pages/404'));
 const MainPage = lazy(() => import('../pages/MainPage'));
@@ -19,7 +21,8 @@ const App = () => {
                         <Routes>
                             <Route path='/' element={<MainPage />} />
                             <Route path='/comics' element={<ComicsPage />} />
-                            <Route path='/comics/:comicId' element={<SingleComicPage />} />
+                            <Route path='/comics/:comicId' element={<SinglePage Component={SingleComicPage} dataType='comic' />} />
+                            <Route path='/characters/:id' element={<SinglePage Component={SingleCharPage} dataType='character' />} />
                             <Route path='*' element={<Page404 />} />
                         </Routes>
                     </Suspense>
